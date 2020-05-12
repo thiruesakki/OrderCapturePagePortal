90ff86fab749f69a55eae879dec0a677d6231340
yurekha <34087864+yurekha@users.noreply.github.com>
2020-05-12 15:03:07 +0530

    Updated Reset html

--- src/main/webapp/ordercapture/js/forgot_password.js
+++ src/main/webapp/ordercapture/js/forgot_password.js
@@ -39,4 +39,55 @@ $('#f_submit').click(function() {
                  });
 
 
+//$('#reset_submit').click(
+//function() {
+//	var url = window.location.href;
+//	var queryString = url ? url.split('?')[1] : window.location.search
+//			.slice(1);
+//	var reset_code = queryString.split('=')[1];
+////	if ($('#resetform').valid()) {
+//		var newPassword = $("#new_password").val();
+//		var confirmPassword = $("#confirm_password").val();
+//		
+//		alert("reset_code"+reset_code);
+//		$.ajax({
+//			type : "POST",
+//			cache: false,
+//			url : "verifyResetPasswordLink?secret=" + reset_code,
+//			contentType : 'application/json',
+//			success : function(data) {
+//				if (data.status == 0) {
+//					var userid = data.errorMessage;
+//					console.log("verification url"+url)
+//					$.ajax({
+//						type : "POST",
+//						cache: false,
+//						url : "updatePassword?reg_id=" + userid
+//								+ "&new_password=" + newPassword,
+//						contentType : 'application/json',
+//						success : function(data) {
+//							if (data.status == 0) {
+//								alert("Updated Successfully");
+//								location.href = "index.jsp";
+//							} else {
+//								alert(data.status);
+//							}
+//						} ,
+//						error : function(msg) {
+//							alert("unable to update the password");
+//						}
+//					});
+//
+//				} else {
+//					alert(data.statusMessage);
+//				}
+//			},
+//			error : function(msg) {
+//				alert("unable to verify reset link");
+//			}
+//		});
+////	}
+//
+//});
+
 });
